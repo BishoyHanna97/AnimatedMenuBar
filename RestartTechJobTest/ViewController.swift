86bb543375ff86f6299466e6dd4db3c2b8256ef9
Bishoy Hanna <BishoyHanna@marcos-macbook-air.local>
2019-08-01 13:36:25 +0200

    Initial Commit

---
+++ RestartTechJobTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RestartTechJobTest
+//
+//  Created by Bishoy Hanna on 8/1/19.
+//  Copyright © 2019 Bishoy Hanna. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
